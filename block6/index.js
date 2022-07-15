463c6f230d6f4eb60aa8ef4160a4e4a0cffe5af9
Larisa <larisa.chikovkina97@gmail.com.com>
2022-07-15 23:44:40 +0300

    fix

--- block6/index.js
+++ block6/index.js
@@ -1,47 +1,13 @@
-const slideList = document.querySelectorAll('.swiper-slide');
+const slideList = document.querySelector('.swiper-wrapper');
 const button = document.querySelector('button');
 
-const isTablet = () => window.innerWidth >= 768 && window.innerWidth < 1120;
-const isDesktop = () => window.innerWidth >= 1120;
-
-/*const slideListHiddn = isTablet() ? document.querySelectorAll('.swiper-slide:nth-last-child(-n + 5)') :
-  isDesktop() ? document.querySelectorAll('.swiper-slide:nth-last-child(-n + 3)') : "";*/
-
-console.log(slideList);
-
-const spoilerSlide = () => {
-  if (isTablet()) {
-    slideList.forEach((slide, index) => index > 5 ?
-      slide.classList.add('slide--hidden') :
-      slide.classList.remove('slide--hidden'))
-  }
-
-  if (isDesktop()) {
-    slideList.forEach((slide, index) => index > 7 ?
-      slide.classList.add('slide--hidden') :
-      slide.classList.remove('slide--hidden'))
-  }
-}
-
 button.addEventListener('click', () => {
-  if (isTablet()) {
-    slideList.forEach((slide, index) => {
-      if (index > 5) slide.classList.toggle('slide--hidden');
-    })
-  }
-  if (isDesktop()) {
-    slideList.forEach((slide, index) => {
-      if (index > 7) slide.classList.toggle('slide--hidden');
-    })
-  }
-
+  slideList.classList.toggle('hidden');
   button.classList.toggle('button--open');
-  button.innerHTML = button.classList.value === "button--open" ? "Скрыть" : "Показать все";
-
+  button.textContent = slideList.classList.contains('hidden') ? 'Скрыть' : 'Показать все';
 });
 
 let init = false;
-
 const swiperCard = () => {
   if (window.innerWidth <= 767) {
     if (!init) {
@@ -64,7 +30,6 @@ const swiperCard = () => {
 }
 
 swiperCard();
-spoilerSlide();
 
 window.addEventListener("resize", () => {
   swiperCard();
